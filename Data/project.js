65558876e5e60b6c2e12c776c56804773ada67d0
sumaiyasheikh7650 <sumaiyaakter1689@gmail.com>
2024-04-01 23:22:09 +0600

    first commit

---
+++ Data/project.js
@@ -0,0 +1,38 @@
+[
+    {
+        "id":1,
+        "images":"https://i.ibb.co/b2zcFKn/image-5.jpg",
+        "Name": "Basma",
+        "Skill": "",
+        "Details": ""
+    },
+    {
+        "id":2,
+        "images": "https://i.ibb.co/FxVys2n/image-2.jpg",
+        "Name": "Basma",
+        "Skill": "",
+        "Details": ""
+    },
+    {
+        "id":3,
+        "images": "https://i.ibb.co/m9WdR1p/image-3.jpg",
+        "Name": "Basma",
+        "Skill": "",
+        "Details": ""
+    },
+    {
+        "id":4,
+        "images": "https://i.ibb.co/bg3Q7gk/image-4.jpg",
+        "Name": "Basma",
+        "Skill": "",
+        "Details": ""
+    },
+    {
+        "id":5,
+        "images": "https://i.ibb.co/Q8FQhWM/image-1.jpg",
+        "Name": "Basma",
+        "Skill": "",
+        "Details": ""
+    }
+  
+]
